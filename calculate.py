3779c70544db4acc62ca9d08bc612559b5e5a1e0
BryanRonad <bryanronad@gmail.com>
2021-10-23 12:36:14 +0530

    Cleaner

--- calculate.py
+++ calculate.py
@@ -1,21 +1,5 @@
 from steam_community_market import Market, AppID
 
-# while True:
-#     dmarket_without_tax = float(input("Enter the dmarket price: "))
-#     dmarket_with_tax = 1.13 * dmarket_without_tax
-#     steam_before_tax =  float(input("Enter the steam price: "))
-#     steam_after_tax = 0.86 * steam_before_tax
-#     profit = ((steam_after_tax-(dmarket_with_tax*99))/steam_after_tax)
-
-#     print("-------------------------------------")
-#     print(f"You'll be paying {round(dmarket_with_tax*99, 2)} on dmarket and getting {round(steam_after_tax, 2)} in the wallet")
-#     print(f"Profit margin = {round(profit*100, 2)}%")
-#     print("-------------------------------------")
-
-#     option = str(input("Wanna check more stuff? "))
-#     if option in ['N', 'n']:
-#         break
-
 market = Market("USD")
 while True:
     item = str(input("Enter exact name of the item: "))
